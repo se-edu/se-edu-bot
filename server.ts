eb1554ec305a091d3adce433fa0dda55613e8942
Paul Tan <pyokagan@gmail.com>
2017-06-13 13:51:17 +0800

    server: implement "Hello world!" web server
    We'll need to implement a web server to respond to GitHub webhook
    events.
    
    As a step towards that goal, let's implement a simple web server using
    Koa[1] that responds with "Hello world!".
    
    Koa is used due to the simplicity of its core concepts (every middleware
    is a function that returns a promise).
    
    In later commits, we will be building on top of this web server code to
    fully flesh out se-edu-bot's behavior, including responding to GitHub
    webhook events.
    
    [1] https://github.com/koajs/koa

--- server.ts
+++ server.ts
@@ -4,6 +4,58 @@ sourceMapSupport.install();
 import dotenv = require('dotenv');
 dotenv.config();
 
+import Koa = require('koa');
+import koaRoute = require('koa-route');
+
+/**
+ * Server app configuration.
+ */
+export interface AppConfig {
+    /**
+     * True if the server is being served behind a proxy.
+     */
+    proxy: boolean;
+}
+
+/**
+ * Creates a Koa Application with the provided `appConfig`.
+ */
+export function createApp(appConfig: AppConfig): Koa {
+    const app = new Koa();
+    app.proxy = appConfig.proxy;
+
+    app.use(koaRoute.get('/', async ctx => {
+        ctx.body = 'Hello world!';
+    }));
+
+    return app;
+}
+
+/**
+ * Extracts {@link AppConfig} from `process.env`.
+ */
+function extractAppConfigFromEnv(): AppConfig {
+    return {
+        proxy: !!extractEnvVar('PROXY', ''),
+    };
+}
+
+/**
+ * Extracts an environment variable from `process.env`.
+ * @throws {Error} Environment variable not defined and `defaultValue` not provided.
+ */
+function extractEnvVar(key: string, defaultValue?: string): string {
+    if (typeof process.env[key] === 'string' && process.env[key]) {
+        return process.env[key];
+    } else if (typeof defaultValue !== 'undefined') {
+        return defaultValue;
+    } else {
+        throw new Error(`$${key} not defined`);
+    }
+}
+
 if (require.main === module) {
-    console.log('Hello world!');
+    const port = parseInt(extractEnvVar('PORT', '5000'), 10);
+    const app = createApp(extractAppConfigFromEnv());
+    app.listen(port);
 }
